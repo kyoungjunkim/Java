4c5ce321afc641d3ce93a9416953739a6f1572ef
sorkcoavus <sorkcoavus@gmail.com>
2022-08-31 10:30:44 +0900

    8/31

---
+++ Ch05/src/sub6/Car.java
@@ -0,0 +1,49 @@
+package sub6;
+
+// 클래스 정의
+
+
+public class Car {
+	
+	
+	
+	// 6개의 멤버로 구성된 car 클래스다.
+	
+	
+	// 자동차의 속성(변수)
+	protected String name;
+	protected String color;
+	protected int speed;
+	//protected는 자식만 참조할수있게함
+	
+	
+	
+	// 생성자
+	public Car( String name, String color, int speed) {
+		this.name = name;
+		this.color = color;
+		this.speed = speed;
+		
+	}
+	
+	
+	
+	
+	// 자동차의 기능(메서드)
+	public void speedUp(int speed) {
+		this.speed += speed;
+		
+	}
+	
+	public void speedDown(int speed) {
+		this.speed -= speed;
+		
+	}
+	public void show() {
+		System.out.println("차랑명 : "+name);
+		System.out.println("차랑색 : "+color);
+		System.out.println("속도 : "+speed);
+	}
+	
+	
+}

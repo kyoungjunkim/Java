8aaf9a4b72d8d8c414e7716d76fc454f863b1654
sorkcoavus <sorkcoavus@gmail.com>
2022-09-20 10:40:15 +0900

    9/20

---
+++ Book1/src/ch05/p153_1.java
@@ -0,0 +1,7 @@
+package ch05;
+
+public class p153_1 {
+	String name;
+	float height;
+	float weight;
+}
---
+++ Book1/src/ch05/p153_2.java
@@ -0,0 +1,8 @@
+package ch05;
+
+public class p153_2 {
+	public static void main(String[] args) {
+		p153_1 personLee = new p153_1();
+	}
+
+}
